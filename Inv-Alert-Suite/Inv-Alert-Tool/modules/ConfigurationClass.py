0a0b53326d3efa30039928e740d5d21a6124c623
“Erick <rerick38@live.com.mx>
2021-07-09 15:12:25 -0500

    latest

--- Inv-Alert-Suite/Inv-Alert-Tool/modules/ConfigurationClass.py
+++ Inv-Alert-Suite/Inv-Alert-Tool/modules/ConfigurationClass.py
@@ -15,16 +15,38 @@ class Configuration:
 	def __init__(self, form_dialog):
 		self.form_dialog = form_dialog
 
+	"""
+	"""
 	def createConfiguration(self):
 		data_conf = []
 		version_es = self.form_dialog.getDataNumberDecimal("Enter the ElasticSearch version:", "7.13")
 		host_es = self.form_dialog.getDataIP("Enter the ElasticSearch IP address:", "localhost")
 		port_es = self.form_dialog.getDataPort("Enter the ElasticSearch listening port:", "9200")
-		folder_inv = self.form_dialog.getDataNameFolderOrFile("Enter the name of the folder where the alert rules will be hosted:", "folder_inv")
-		use_ssl = self.form_dialog.getDataYesOrNo("\nDo you want Telk-Alert to connect to ElasticSearch using the SSL/TLS protocol?", "Connection Via SSL/TLS")
+		folder_inv = self.form_dialog.getDataNameFolderOrFile("Enter the name of the folder where the inventories created will be saved:", "folder_inv")
+		use_ssl = self.form_dialog.getDataYesOrNo("\nDo you want Inv-Alert to connect to ElasticSearch using the SSL/TLS protocol?", "Connection Via SSL/TLS")
 		data_conf.append(version_es)
 		data_conf.append(host_es)
 		data_conf.append(port_es)
 		data_conf.append(folder_inv)
+		if use_ssl == "ok":
+			data_conf.append(True)
+			valid_certificate = self.form_dialog.getDataYesOrNo("\nDo you want the certificate for SSL/TLS communication to be validated?", "Certificate Validation")
+			if valid_certificate == "ok":
+				data_conf.append(True)
+				cert_file = self.form_dialog.getFileOrDirectory('/etc/Inv-Alert-Suite/Inv-Alert', "Select the CA certificate:")
+				data_conf.append(cert_file)
+			else:
+				data_conf.append(False)
+		else:
+			data_conf.append(False)
+		http_auth = self.form_dialog.getDataYesOrNo("\nIs it required to enable the use of HTTP authentication to connect to ElasticSearch?", "HTTP Authentication")
+		if http_auth == "ok":
+			data_conf.append(True)
+			user_http_auth = self.utils.encryptAES(self.form_dialog.getDataInputText("Enter the username for HTTP authentication:", "user_http"))
+			pass_http_auth = self.utils.encryptAES(self.form_dialog.getDataPassword("Enter the user's password for HTTP authentication:", "password"))
+			data_conf.append(user_http_auth)
+			data_conf.append(pass_http_auth)
+		else:
+			data_conf.append(False)
 		print(data_conf)
 		self.form_dialog.mainMenu()
